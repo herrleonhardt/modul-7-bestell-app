bbc14412a518d0320b9c5bfc48d539ec9a137952
Stefan Leonhardt <leonhardt.stefan@googlemail.com>
2026-07-01 14:12:15 +0200

    tidy up basket events

--- js/components/basket/basket.events.js
+++ js/components/basket/basket.events.js
@@ -1,9 +1,18 @@
 import { initApp } from "../../app/init.js";
 import { updateProductCardFooterById } from "../product/product.ui.js";
-import { isVoucherValid, requestAddProductToBasketById, requestClearBasket, requestDecreaseProductAmmountInBasketById, requestOrder, requestRemoveProductFromBasketById, setActiveVoucher,} from "./basket.js";
-import { updateVoucherValue, toggleBasket, updateBasketContent, hideBasketInstandly, renderBasketContent,} from "./basket.ui.js";
+import { isVoucherValid, requestAddProductToBasketById, requestClearBasket, requestDecreaseProductAmmountInBasketById, requestOrder, requestRemoveProductFromBasketById, setActiveVoucher} from "./basket.js";
+import { updateVoucherValue, toggleBasket, updateBasketContent, hideBasketInstandly, renderBasketContent} from "./basket.ui.js";
 
 export function initBasketEvents() {
+  initToggleBasketEvent();
+  initDeleteEntryButtonEvent();
+  initDecreaseEntryAmmountButtonEvent();
+  initAddEntryButtonEvent();
+  initOrderButtonEvent();
+  initVoucherInputFocusoutEvent();
+}
+
+function initToggleBasketEvent() {
   document.addEventListener("click", (e) => {
     const basketToggleButton = e.target.closest(
       "div[data-action='toggle-basket']",
@@ -11,7 +20,11 @@ export function initBasketEvents() {
     if (basketToggleButton) {
       toggleBasket();
     }
+  });
+}
 
+function initDeleteEntryButtonEvent() {
+  document.addEventListener("click", (e) => {
     const deleteEntryButton = e.target.closest("[data-action='delete-entry']");
     if (deleteEntryButton) {
       const entry = deleteEntryButton.closest("article");
@@ -20,6 +33,11 @@ export function initBasketEvents() {
       updateProductCardFooterById(entryId);
       updateBasketContent();
     }
+  });
+}
+
+function initDecreaseEntryAmmountButtonEvent() {
+  document.addEventListener("click", (e) => {
     const decreaseEntryAmmountButton = e.target.closest(
       "[data-action='decrease-entry']",
     );
@@ -30,7 +48,11 @@ export function initBasketEvents() {
       updateProductCardFooterById(entryId);
       updateBasketContent();
     }
+  });
+}
 
+function initAddEntryButtonEvent() {
+  document.addEventListener("click", (e) => {
     const addEntryButton = e.target.closest("[data-action='increase-entry']");
     if (addEntryButton) {
       const entry = addEntryButton.closest("article");
@@ -39,7 +61,11 @@ export function initBasketEvents() {
       updateProductCardFooterById(entryId);
       updateBasketContent();
     }
+  });
+}
 
+function initOrderButtonEvent() {
+  document.addEventListener("click", (e) => {
     const orderBtn = e.target.closest("button#order-btn");
     if (orderBtn) {
       requestOrder();
@@ -51,9 +77,10 @@ export function initBasketEvents() {
       }, 5000);
     }
   });
+}
 
+function initVoucherInputFocusoutEvent() {
   document.addEventListener("focusout", (e) => {
-
     if (e.target.id == "voucher-code") {
       let voucherCode = e.target.value;
       if (isVoucherValid(voucherCode)) {
